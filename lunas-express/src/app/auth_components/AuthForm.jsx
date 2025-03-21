d0d39c122e87379ac2aa8d7eb27c5f41f3e6fcd9
JamesBillate <james.billate@ciit.edu.ph>
2025-03-21 19:11:14 +0800

    Forgot Password UI

--- lunas-express/src/app/auth_components/AuthForm.jsx
+++ lunas-express/src/app/auth_components/AuthForm.jsx
@@ -7,22 +7,28 @@ export default function AuthForm({ title, buttonText, authMode }) {
         <div className="px-10 py-12 bg-blue-300 w-[25rem] rounded-xl">
           <form className="grid">
             <h1 className="text-2xl font-bold">{title}</h1>
-            <EntryField
-              type="text"
-              label="Email"
-              placeholder="you@example.com"
-            />
+            {authMode === "login" || authMode === "signup" ? (
+              <EntryField
+                type="text"
+                label="Email"
+                placeholder="you@example.com"
+              />
+            ) : (
+              ""
+            )}
             <EntryField
               type="password"
               label="Password"
               placeholder="Enter 6 characters or more"
             />
-            {authMode === "signup" && (
+            {authMode === "signup" || authMode === "forgotpw" ? (
               <EntryField
                 type="password"
                 label="Re-enter Password"
                 placeholder="Enter 6 characters or more"
               />
+            ) : (
+              ""
             )}
             {authMode === "login" && (
               <div className="mt-2 text-right text-sm cursor-pointer">
@@ -31,7 +37,11 @@ export default function AuthForm({ title, buttonText, authMode }) {
             )}
             <div className="p-2"></div>
             <Button type="submit" label={buttonText} color="bg-blue-600" />
-            <Button type="button" label="Google" color="bg-red-600" />
+            {authMode === "login" || authMode === "signup" ? (
+              <Button type="button" label="Google" color="bg-red-600" />
+            ) : (
+              ""
+            )}
             {authMode === "signup" && (
               <div className="mt-10 text-center text-sm">
                 Already have an account?
