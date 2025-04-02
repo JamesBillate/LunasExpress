504ef22500e129f5342f4d0a993f0661e2c936d2
JamesBillate <james.billate@ciit.edu.ph>
2025-04-02 19:18:19 +0800

    CR for Listing

--- lunas-express/src/app/firebase/config.jsx
+++ lunas-express/src/app/firebase/config.jsx
@@ -1,5 +1,6 @@
 import { getApps, initializeApp, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -11,8 +12,9 @@ const firebaseConfig = {
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
 
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-export { app, auth, provider };
+export { db, app, auth, provider, collection, getDocs };
