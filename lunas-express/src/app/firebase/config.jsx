c06a99122ed11f5dc12135d26378e2107d5c6b26
Niko <nikolai.cabradilla@gmail.com>
2025-04-04 14:53:01 +0800

    exported db to test

--- lunas-express/src/app/firebase/config.jsx
+++ lunas-express/src/app/firebase/config.jsx
@@ -1,5 +1,6 @@
 import { getApps, initializeApp, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -14,5 +15,6 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
 
-export { app, auth, provider };
+export { app, auth, provider, db,};
